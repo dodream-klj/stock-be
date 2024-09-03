b63a6bd5632997e2f68f33768d159f2fb8c51b52
jade-jg <jade.jg@kakaocorp.com>
2024-09-03 17:37:09 +0900

    프로젝트 최초 업로드

---
+++ src/test/java/com/soongsil/klj/stockbe/StockBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soongsil.klj.stockbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
